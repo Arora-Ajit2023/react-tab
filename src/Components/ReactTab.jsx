f4f7059296949287080dc8210a746da448184663
Arora-Ajit <aroraajit302@gmail.com>
2023-06-14 08:17:28 +0530

    first commit

---
+++ src/Components/ReactTab.jsx
@@ -0,0 +1,63 @@
+// @ts-nocheck
+
+import React, { useState } from 'react'
+import './ReactTab.css'
+
+const TabData = [
+    [
+        "React is extremely popular",
+        "It makes building complex, interactive UIs a breeze",
+        "It's powerful & flexible",
+        "It has a very active and versatile ecosystem"
+    ],
+    [
+        "Components, JSX & Props",
+        "State",
+        "Hooks (e.g., useEffect())",
+        "Dynamic rendering"
+    ],
+    [
+        "Official web page (react.dev)",
+        "Next.js (Fullstack framework)",
+        "React Native (build native mobile apps with React)"
+    ]
+]
+
+
+const ReactTab = () => {
+    const [activeContentIndex, setActiveContentIndex] = useState(0)
+
+
+    return (
+        <div>
+            <header>
+                <img src="https://uc08fv.csb.app/react-logo-xs.png" alt="react-logo" />
+                <div>
+                    <h1>
+                        React.js
+                    </h1>
+                    <p>i.e., using the React library for rendering the UI</p>
+                </div>
+            </header>
+            <div id='tabs'>
+                <div className='btns'>
+                    <button className={activeContentIndex === 0 ? 'active' : ''} onClick={() => setActiveContentIndex(0)}>Why React?</button>
+                    <button className={activeContentIndex === 1 ? 'active' : ''} onClick={() => setActiveContentIndex(1)}>Core Features</button>
+                    <button className={activeContentIndex === 2 ? 'active' : ''} onClick={() => setActiveContentIndex(2)}>Related Resources</button>
+                </div>
+                <div id="content">
+                    <ul>
+                        {TabData[activeContentIndex].map((data) => {
+                            return (
+                                <li key={data}>{data}</li>
+                            )
+                        })}
+                    </ul>
+
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default ReactTab
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+// @ts-nocheck
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ReactTab from './Components/ReactTab.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ReactTab />
+  </React.StrictMode>,
+)
