da9536196a02c41b34b8acea8bb1c9d2121b8fa7
Arora-Ajit <aroraajit302@gmail.com>
2023-06-14 23:05:49 +0530

    change folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 // @ts-nocheck
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ReactTab from './Components/ReactTab/ReactTab.jsx'
-import ParentToChildProp from './Components/ParentToChildProp.jsx'
+
+import ReactTab from './Components/ReactTab/React.Tabs/ReactTab.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ParentToChildProp />
+    <ReactTab />
   </React.StrictMode>,
 )
