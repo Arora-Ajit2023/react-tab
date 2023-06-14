6d49009c299d220c036869403e7d774cc1faa823
Arora-Ajit <aroraajit302@gmail.com>
2023-06-14 23:00:21 +0530

    change path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ReactTab from './Components/ReactTab.jsx'
+import ReactTab from './Components/ReactTab/ReactTab.jsx'
 import ParentToChildProp from './Components/ParentToChildProp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
